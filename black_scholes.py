38cd0bc90b5385f83e9f9a52280aa83ce8a644a5
Alexander <ambollinger0@gmail.com>
2026-07-20 16:00:15 -0700

    Add d2 function

--- black_scholes.py
+++ black_scholes.py
@@ -4,5 +4,10 @@ def d1(S, K, T, r, sigma):
     numerator = np.log(S/K) + (r + sigma**2/2)*T
     denominator = sigma * np.sqrt(T)
     return numerator / denominator
+def d2(S, K, T, r, sigma):
+    numerator = d1(S, K, T, r, sigma) - sigma*np.sqrt(T)
+    return numerator
+
 if __name__ == '__main__':
     print(d1(100, 100, 1, 0.05, 0.2))
+    print(d2(100, 100, 1, 0.05, 0.2))
