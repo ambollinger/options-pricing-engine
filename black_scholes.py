323110b3f1e127c9740f810a2ed024bab4c1c6af
Alexander <ambollinger0@gmail.com>
2026-07-20 16:37:40 -0700

    Add call_price function

--- black_scholes.py
+++ black_scholes.py
@@ -7,7 +7,13 @@ def d1(S, K, T, r, sigma):
 def d2(S, K, T, r, sigma):
     numerator = d1(S, K, T, r, sigma) - sigma*np.sqrt(T)
     return numerator
+def call_price(S, K, T, r, sigma):
+    N_d1 = norm.cdf(d1(S, K, T, r, sigma))
+    N_d2 = norm.cdf(d2(S, K, T, r, sigma))
+    price = S * N_d1 - K * np.exp(-r*T) * N_d2
+    return price
 
 if __name__ == '__main__':
     print(d1(100, 100, 1, 0.05, 0.2))
     print(d2(100, 100, 1, 0.05, 0.2))
+    print(call_price(100, 100, 1, 0.05, 0.2))
