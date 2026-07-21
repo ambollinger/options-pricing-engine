2dfaad62c79d462ea1ccde626f83c32bb6f3012c
Alexander <ambollinger0@gmail.com>
2026-07-20 21:10:40 -0700

    Add call_payoff function

--- binomial_tree.py
+++ binomial_tree.py
@@ -13,7 +13,12 @@ def stock_price_tree(S, u, d, n):
     return prices
 def risk_neutral_probability(r, dt, u,d):
     return (np.exp(r * dt) - d) / (u -d)
-
+def call_payoff(prices, K):
+    payoffs = []
+    for price in prices:
+        payoff = max(price - K, 0)
+        payoffs.append(payoff)
+    return payoffs
 
 
 
@@ -23,3 +28,4 @@ if __name__ == "__main__":
     print(up_down_factors(0.2,0.01))
     print(stock_price_tree(100, 1.02, .98, 3))
     print(risk_neutral_probability(0.05, 0.01, 1.02, .98))
+    print(call_payoff([94.12, 97.96, 101.96, 106.12], 100))
