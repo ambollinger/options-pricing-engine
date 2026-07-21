8865f125a0fe2480077e495d8f12b6860875d71a
Alexander <ambollinger0@gmail.com>
2026-07-20 21:20:15 -0700

    Add binomial_price function

--- binomial_tree.py
+++ binomial_tree.py
@@ -19,6 +19,22 @@ def call_payoff(prices, K):
         payoff = max(price - K, 0)
         payoffs.append(payoff)
     return payoffs
+def binomial_price(S, K, T, r, sigma, n):
+    dt = T / n
+    u, d = up_down_factors(sigma, dt)
+    p = risk_neutral_probability(r, dt, u, d)
+
+    prices = stock_price_tree(S, u, d, n)
+    values = call_payoff(prices, K)
+
+    for step in range(n):
+        new_values = []
+        for i in range(len(values) - 1 ):
+            value = np.exp(-r*dt) * (p * values[i+1] + (1-p) * values[i])
+            new_values.append(value)
+        values = new_values
+    return values[0]
+
 
 
 
@@ -29,3 +45,5 @@ if __name__ == "__main__":
     print(stock_price_tree(100, 1.02, .98, 3))
     print(risk_neutral_probability(0.05, 0.01, 1.02, .98))
     print(call_payoff([94.12, 97.96, 101.96, 106.12], 100))
+    print(binomial_price(100, 100, 1, 0.05, 0.2, 100))
+
