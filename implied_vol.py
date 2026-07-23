b1dcc71fdb81146adfb50bca50678a2f0ca7d64e
Alexander <ambollinger0@gmail.com>
2026-07-22 21:42:31 -0700

    Pull SPY options expirations via yfinance

--- implied_vol.py
+++ implied_vol.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.optimize import brentq
 from black_scholes import call_price
+import yfinance as yf
+
 
 def price_difference(sigma, S, K, T, r, market_price):
     model_price = call_price(S, K, T, r, sigma)
@@ -14,3 +16,7 @@ if __name__ == "__main__":
     test_price = call_price(100, 100, 1, 0.05, 0.2)
     print(test_price)
     print(find_implied_vol(100, 100, 1, 0.05, test_price))
+
+    ticker = yf.Ticker("SPY")
+    expirations = ticker.options
+    print(expirations)
